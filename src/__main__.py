79c2816a5e212becda88e391257f5356e6ced341
Vaidehi Pujary <vaipujary@gmail.com>
2024-03-16 20:03:26 -0700

    GUI updates

--- src/__main__.py
+++ src/__main__.py
@@ -2,11 +2,18 @@ import pygame
 import sys
 import json
 from word_search_generator import WordSearch
+from tkinter import *
+from tkinter import messagebox
+import tkinter as tk
+from pgu import gui
 import math
+import collections
+#from PyQt5.QtWidgets import QMessageBox
 
 from util.colors import Color
 from gemini.ai import words_related_to_theme
 from algo.lazy import calculate_minimum_grid_size_with_buffer
+
 # Initialize Pygame
 pygame.init()
 
@@ -31,9 +38,10 @@ puzzle_data = json.loads(puzzle_data)
 grid = puzzle_data['puzzle']
 # print(grid)
 words = puzzle_data['words']
+words = [x.lower() for x in words]
 # print(words)
 # Font for rendering text
-font = pygame.font.Font("assets/AovelSansRounded-rdDL.ttf", 36)
+font = pygame.font.Font("assets/Horizon Type - AcherusGrotesque-Regular.otf", 36)
 
 # Keep track of the start cell, end cell, and if we're currently selecting
 start_cell = None
@@ -49,20 +57,59 @@ grid_init_x = window_width / 4  # set the initial x position of the grid
 grid_init_y = window_height / 4  # set the initial y position of the grid
 strikethrough = False
 selected_word = ''
-strikethrough_word_indices = []
+selected_words = []  # List to keep track of the words selected by the user
+strikethrough_word_indices = []  # List to keep track of indices of the completed words
 
 
-def get_font(size): # Returns Press-Start-2P in the desired size
+# Class widget or pop-up window
+class POPDialog(gui.Dialog):
+
+    # Class Constructor with variable-length Arguments
+    def __init__(self, value, **params):
+        # Title of the widget
+        title = gui.Label("Popup Box")
+
+        # Table in the Widget of size 100X70
+        main = gui.Table(width=100, height=70)
+
+        # Table Content or sample text
+        label = gui.Label("This is a sample pop up!")
+
+        # Ok Button
+        btn = gui.Button("Ok")
+
+        # Click Event and self.close Connection will
+        # terminate the pop-up when Click event will perform
+        btn.connect(gui.CLICK, self.close, None)
+
+        # Next row is created in table
+        main.tr()
+
+        # Label is added in the TD Container of the row
+        main.td(label)
+
+        # Next row
+        main.tr()
+
+        # Next row
+        main.tr()
+
+        # Adding the button in the TD container
+        main.td(btn)
+        # Initializing the Constructor
+        gui.Dialog.__init__(self, title, main)
+
+
+def get_font(size):  # Returns Press-Start-2P in the desired size
     return pygame.font.Font("assets/font.ttf", size)
 
 
 # Display the heading
-HEAD_TEXT = get_font(20).render("Generative Word Search", True, '#9B56E3')
-HEAD_TEXT_RECT = HEAD_TEXT.get_rect(center=(350, 100))
+#HEAD_TEXT = get_font(20).render("Generative Word Search", True, '#9B56E3')
+#HEAD_TEXT_RECT = HEAD_TEXT.get_rect(center=(350, 100))
 
 
 def draw_grid(grid, hover_cell):
-
     for y, row in enumerate(grid):
         for x, letter in enumerate(row):
             rect = pygame.Rect(grid_init_x + x * BLOCK_SIZE, grid_init_y + y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE)
@@ -85,6 +132,7 @@ def add_rect(start_x, start_y):
 
 
 def add_text(words, start_x, start_y, strikethrough, selected_word):
+    font = pygame.font.Font("assets/Horizon Type - AcherusGrotesque-Regular.otf", 25)
 
     for idx, word in enumerate(words):
         if (strikethrough and word.lower() == selected_word) or (idx in strikethrough_word_indices):
@@ -92,7 +140,7 @@ def add_text(words, start_x, start_y, strikethrough, selected_word):
                 strikethrough_word_indices.append(idx)
             text_surface = font.render(word, True, Color.BLACK.value)
             text_rect = text_surface.get_rect()
-            text_rect.topleft = (start_x + 50, start_y + 25)
+            text_rect.topleft = (start_x + 60, start_y + 25)
             # Calculate the position of the line for strikethrough effect
             line_y = text_rect.centery
             # Draw the text
@@ -100,8 +148,8 @@ def add_text(words, start_x, start_y, strikethrough, selected_word):
             # Draw the strikethrough line
             pygame.draw.line(screen, Color.BLACK.value, (text_rect.left, line_y), (text_rect.right, line_y), 2)
         else:
-            text = font.render(word, True, Color.RED.value)
-            screen.blit(text, (start_x + 50, start_y + 25))
+            text = font.render(word, True, Color.BLACK.value)
+            screen.blit(text, (start_x + 60, start_y + 25))
 
         start_y += 40
 
@@ -133,7 +181,7 @@ def compute_path(start_cell, end_cell):
     delta_col = end_cell[1] - start_cell[1]
     step_row = delta_row // max(abs(delta_row), 1)
     step_col = delta_col // max(abs(delta_col), 1)
-    
+
     for i in range(int(max(abs(delta_row), abs(delta_col)) + 1)):
         path.append((start_cell[0] + i * step_row, start_cell[1] + i * step_col))
     return path
@@ -145,8 +193,30 @@ def is_valid_word(path):
     print(selected_word, searchable_words)
     return selected_word in searchable_words
 
-# def game_over():
 
+def game_over():
+    # empty = gui.Container(width=100, height=100)
+    # gui.init(empty)
+    # Initial Application is created with default theme
+    app = gui.Desktop()
+    # Connecting the Quit event with app.quit function,
+    # which will terminate the gui execution
+    app.connect(gui.QUIT, app.quit, None)
+    # Initial Table
+    c = gui.Table(width=320, height=240)
+    # Object of the Class or widget
+    dialog = POPDialog("#00ffff")
+    # Click me button
+    e = gui.Button("Click Me")
+    # Connection Click event and open widget function,
+    # which will open the class widget or pop-up
+    e.connect(gui.CLICK, dialog.open, None)
+    # next row
+    c.tr()
+    # adding the connection in TD container
+    c.td(e)
+    # Running the initial application
+    app.run(c)
 
 running = True
 while running:
@@ -181,6 +251,8 @@ while running:
             if is_valid_word(selected_cells):
                 final_selected_cells = list(selected_cells)
                 selected_word = ''.join(grid[y][x] for y, x in selected_cells).lower()
+                selected_words.append(selected_word)
+                selected_words = [x.upper() for x in selected_words]
                 strikethrough = True
             else:
                 final_selected_cells.clear()
@@ -191,10 +263,13 @@ while running:
     else:
         selected_cells = []  # Clear temporary selection path when not selecting
 
-    screen.fill((151, 161, 219))
-    screen.blit(HEAD_TEXT, HEAD_TEXT_RECT)
+    screen.fill(Color.WHITE.value)
+    #screen.blit(HEAD_TEXT, HEAD_TEXT_RECT)
     draw_grid(grid, selected_cells)  # Pass the temporary path for visualization
     draw_words(words, strikethrough, selected_word)
     pygame.display.flip()
+    if set(selected_words) == set(words):
+        game_over()
+
 
 pygame.quit()
